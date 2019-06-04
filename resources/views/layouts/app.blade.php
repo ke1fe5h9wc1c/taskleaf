34f72f43bc5078a0a8cf21c2042d593ebb804faa
鵜澤聡 <fd5780@yahoo.co.jp>
2019-06-05 08:38:56 +0900

    boot及び、DB導入

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>アプリ名 - @yield('title')</title>
+  </head>
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-light mb-2"style="background-color:#e3f2fd;">
+        <a href="#" class="navbar-brand">Custom</a>
+      </nav>
+      @yield('content')
+    </div>
+  </body>
+</html>
