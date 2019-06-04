7a033f02ed93725dd781544677e8c52470546b3a
鵜澤聡 <fd5780@yahoo.co.jp>
2019-06-05 08:56:00 +0900

    navbarまで

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,19 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- jqueryの読み込み -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- BootstrapのJS読み込み -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
 <html>
   <head>
     <title>アプリ名 - @yield('title')</title>
   </head>
   <body>
+    <nav class="navbar navbar-light mb-2" style="background-color:#e3f2fd;">
+      <a href="#" class="navbar-brand">Taskleaf</a>
+    </nav>
+
     <div class="container">
-      <nav class="navbar navbar-light mb-2"style="background-color:#e3f2fd;">
-        <a href="#" class="navbar-brand">Custom</a>
-      </nav>
       @yield('content')
     </div>
   </body>
